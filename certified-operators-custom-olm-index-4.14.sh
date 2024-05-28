286d0a838069d55643588b5c7dcf80b1271606aa
Borball Zhai <borball.zh@gmail.com>
2024-05-28 18:43:02 -0400

    add script for certified-operators index

--- certified-operators-custom-olm-index-4.14.sh
+++ certified-operators-custom-olm-index-4.14.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-OCP_VERSION=4.12
-INDEX_NAME=redhat-operator-index
+OCP_VERSION=4.14
+INDEX_NAME=certified-operator-index
 #example: registry.redhat.io/redhat/redhat-operator-index:v4.12
 INDEX_LOCATION=registry.redhat.io/redhat/$INDEX_NAME:v$OCP_VERSION
 LOCAL_REGISTRY="hub-helper:5000"
 LOCAL_REPOSITORY="operators"
 
 declare -A OPERATORS=(
-  ["ptp-operator"]="ptp-operator.4.12.0-202402081808"
-  ["local-storage-operator"]="local-storage-operator.v4.12.0-202403082008"
-  ["sriov-network-operator"]="sriov-network-operator.v4.12.0-202402081808"
+  ["sriov-fec"]="sriov-fec.v2.7.2"
+  ["datadog-operator-certified"]="datadog-operator.v1.5.0"
+  ["vault-secrets-operator"]="vault-secrets-operator.v0.6.0"
   )
 
 basedir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -54,8 +54,6 @@ build_index_image(){
 build_index(){
   export operator=$1
   export max_version=$2
-  #example: ptp-operator.v4.14.0-200000000000, in order to fix the issue that sriov-network-operator 4.13 bundles exists in 4.14 index
-  export minimal_version=$(echo $max_version|sed -E "s/([0-9]{12})/200000000000/")
   local op_workspace="$workspace/$operator"/"$max_version"
   mkdir -p "$op_workspace"
 
@@ -70,10 +68,13 @@ build_index(){
   #fetch the name/skipRange of the one desired to keep
   local entry_name_keep=$(yq '.entries[] | select(.name == env(max_version))|.name' "$op_workspace"/olm-channel-original.yaml)
   local entry_skipRange_keep=$(yq '.entries[] | select(.name == env(max_version))|.skipRange' "$op_workspace"/olm-channel-original.yaml)
+  local entry_replace_keep=$(yq '.entries[] | select(.name == env(max_version))|.replaces' "$op_workspace"/olm-channel-original.yaml)
 
   #fetch the name/skipRange of the latest one on the index
-  local entry_name_latest=$(yq '.entries[] | select(has("skips"))|.name' "$op_workspace"/olm-channel-original.yaml)
-  local entry_skipRange_latest=$(yq '.entries[] | select(has("skips"))|.skipRange' "$op_workspace"/olm-channel-original.yaml)
+  export entry_name_latest=$(yq '.entries[].name' "$op_workspace"/olm-channel-original.yaml |sort -r |head -n1)
+  export min_version=$(yq '.entries[].name' "$op_workspace"/olm-channel-original.yaml |sort |head -n1)
+  local entry_skipRange_latest=$(yq '.entries[]|select(.name == env(entry_name_latest)) |.skipRange' "$op_workspace"/olm-channel-original.yaml)
+  local entry_replaces_latest=$(yq '.entries[]|select(.name == env(entry_name_latest)) |.replaces' "$op_workspace"/olm-channel-original.yaml)
 
   if [[ "$entry_name_keep" == "$entry_name_latest" ]]; then
     echo "[$operator]: $entry_name_keep is the latest one, no need update olm-channel.yaml."
@@ -86,17 +87,21 @@ build_index(){
     echo "[$operator]: patching \"latest\" to point instead of $entry_name_latest to $max_version in olm-channel."
     sed  "s/name: ${entry_name_latest}/name: ${entry_name_keep}/g" "$op_workspace"/olm-channel-updated-0.yaml > "$op_workspace"/olm-channel-updated-1.yaml
     sed  "s/skipRange: '${entry_skipRange_latest}'/skipRange: '${entry_skipRange_keep}'/g" "$op_workspace"/olm-channel-updated-1.yaml > "$op_workspace"/olm-channel-updated-2.yaml
+    #some operator doesn't have '' in the skipRange
+    sed  "s/skipRange: ${entry_skipRange_latest}/skipRange: '${entry_skipRange_keep}'/g" "$op_workspace"/olm-channel-updated-2.yaml > "$op_workspace"/olm-channel-updated-3.yaml
+    sed  "s/replaces: '${entry_skipRange_latest}'/replaces: '${entry_replace_keep}'/g" "$op_workspace"/olm-channel-updated-3.yaml > "$op_workspace"/olm-channel-updated-4.yaml
+    sed  "s/replaces: ${entry_replaces_latest}/replaces: ${entry_replace_keep}/g" "$op_workspace"/olm-channel-updated-4.yaml > "$op_workspace"/olm-channel-updated-5.yaml
 
     echo "[$operator]: deleting entries whose version is greater than $max_version in olm-channel."
-    yq 'del( .entries[] |select(.name > env(max_version)))' "$op_workspace"/olm-channel-updated-2.yaml > "$op_workspace"/olm-channel-updated-3.yaml
+    yq 'del( .entries[] |select(.name > env(max_version)))' "$op_workspace"/olm-channel-updated-5.yaml > "$op_workspace"/olm-channel-updated-6.yaml
 
     echo "[$operator]: deleting versions greater than $max_version from the skips in olm-channel."
-    yq '.entries[]| select(.name == env(max_version)).skips |del(.[] | select(. >= env(max_version)))|parent|parent|parent' "$op_workspace"/olm-channel-updated-3.yaml  > "$op_workspace"/olm-channel.yaml
+    yq '.entries[]| select(.name == env(max_version)).skips |del(.[] | select(. >= env(max_version)))|parent|parent|parent' "$op_workspace"/olm-channel-updated-6.yaml  > "$op_workspace"/olm-channel.yaml
   fi
 
   echo "[$operator]: generating olm-bundles version <= $max_version."
   #get olm.bundles lower than $max_version
-  yq '. | select(.schema=="olm.bundle" and .package == env(operator) and .name > env(minimal_version) and .name <= env(max_version))' "$full_index" > "$op_workspace"/olm-bundles.yaml
+  yq '. | select(.schema=="olm.bundle" and .package == env(operator) and .name >= env(min_version) and .name <= env(max_version))' "$full_index" > "$op_workspace"/olm-bundles.yaml
 
   echo "---" >> $tuned_index
   cat "$op_workspace"/olm-package.yaml >> $tuned_index
